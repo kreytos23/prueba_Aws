fda1a097ac71727b075245916a38fe55106bc6d7
Cesar Martin <kreytos.pavi10@gmail.com>
2024-06-30 03:03:33 -0600

    Update application.py

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hola_mundo():
-  return 'Hola Mundo!'
+  return 'Hola Mundo! in AWS'
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", port=80, debug=True)
